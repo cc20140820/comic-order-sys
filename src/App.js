406e63d21623f4f816afe3d18d35dd994c361369
Nero_chw <510448079@qq.com>
2020-03-05 20:53:58 +0800

    动效加入

--- src/App.js
+++ src/App.js
@@ -7,15 +7,34 @@ function App() {
     <div className='App'>
       <header className='top_area'>
         <div className='lantern_area'>
-          <Lantern type={'left'} />
+          {/* <Lantern type={'left'} />
           <Lantern type={'mid'} />
-          <Lantern type={'right'} />
+          <Lantern type={'right'} /> */}
         </div>
-        <div className='tag_area'>
-          
-        </div>
+        <img className='tag' src={require('./images/tags.png')} alt='tag' />
       </header>
-      
+      <div className='couple'>
+        <img src={require('./images/boy.gif')} alt='boy' />
+        <img src={require('./images/girl.gif')} alt='girl' />
+      </div>
+      <div className='drink'>
+        <img src={require('./images/drink_tea.gif')} alt='drink' />
+      </div>
+      <div className='pot'>
+        <img src={require('./images/pot.gif')} alt='pot' />
+      </div>
+      <div className='cook'>
+        <img src={require('./images/cook.gif')} alt='cook' />
+      </div>
+
+
+      <div className='dancing'>
+        <div className='dancing_wrap'>
+          <img src={require('./images/dancer.gif')} alt='dancer' />
+          <img src={require('./images/dancer2.gif')} alt='dancer2' />
+          <img src={require('./images/music.gif')} alt='music' />
+        </div>
+      </div>
     </div>
   );
 }
