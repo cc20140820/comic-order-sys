176684ab7686bf64f292a127b96d65a28848f68d
Nero_chw <510448079@qq.com>
2020-03-06 16:19:54 +0800

    动画完成

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import './home.css';
-import Lantern from './components/lantern';
 
 function App() {
   return (
     <div className='App'>
       <header className='top_area'>
-        <div className='lantern_area'>
-          {/* <Lantern type={'left'} />
-          <Lantern type={'mid'} />
-          <Lantern type={'right'} /> */}
-        </div>
+        <img className='lantern' src={require('./images/lantern.png')} alt='tag' />
         <img className='tag' src={require('./images/tags.png')} alt='tag' />
       </header>
       <div className='couple'>
@@ -27,6 +22,15 @@ function App() {
         <img src={require('./images/cook.gif')} alt='cook' />
       </div>
 
+      <div className='ready'>
+        <img src={require('./images/ready.gif')} alt='ready' />
+      </div>
+      <div className='welcome'>
+        <img src={require('./images/welcome.gif')} alt='welcome' />
+      </div>
+      <div className='run'>
+        <img src={require('./images/run.gif')} alt='run' />
+      </div>
 
       <div className='dancing'>
         <div className='dancing_wrap'>
